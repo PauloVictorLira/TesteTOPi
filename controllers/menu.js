37b741d39b07e615e1b1d528c90fd3d296ad517e
PauloVictorLira <paulovictorlira@hotmail.com>
2021-07-04 14:51:35 -0300

    disable mongoose

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,16 +1,17 @@
 //importa o pacote mongoose
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 //instancia a tabela meal
-const menu = mongoose.model("meal")
+//const menu = mongoose.model("meal")
 
 //torna o próprio arquivo (menu.js) em um pacote "exportável"
 module.exports = {
     
     //função "index", responde uma msg
     async index(req, res) {
-        const meals = await menu.find(req.body);
-        return res.json(meals)
+        //const meals = await menu.find(req.body);
+        //return res.json(meals)
+        return res.json({"teste":2})
     }
 
 };
--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const express = require("express");
 
 //importa o pacote mongoose
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 //instancia o express
 const app = express();
 
 //conexão BD
-mongoose.connect("mongodb+srv://admin:admin@cluster0.4sjwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+//mongoose.connect("mongodb+srv://admin:admin@cluster0.4sjwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 //responde com uma msg no browser
 app.use("/", require("./src/routes"));
